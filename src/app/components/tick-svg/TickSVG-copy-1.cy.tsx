5720d5dc9b2817da2a6691fd885d5f930dedb2d5
Kiran Boyle <anthony.kiran.boyle@gmail.com>
2023-10-02 12:07:52 +0200

    address vulnerability

--- src/app/components/tick-svg/TickSVG-copy-1.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import TickSVG from './TickSVG'
-
-describe('<TickSVG />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<TickSVG />)
-  })
-})
